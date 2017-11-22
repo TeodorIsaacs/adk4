4b64bacb68fec06cb439dc9a54692556c6603254
Teodor Hurtigh Isaacs <Teodor.hurtigh@gmail.com>
2017-11-22 10:20:20 +0100

    Gjorde den lite snabbare

--- src/Reducer.java
+++ src/Reducer.java
@@ -4,13 +4,11 @@ public class Reducer {
 
     Kattio io;
     private int v, e, c, nORoles, nOScenes, nOactors;
-    private Edge[] edges;
     private ArrayList<Integer> nonSoloVerticies;
-
+    private StringBuilder boi;
     public static void main(String[] args) {
         Kattio io = new Kattio(System.in, System.out);
         Reducer reducer = new Reducer(io);
-        reducer.print();
     }
 
 
@@ -24,70 +22,71 @@ public class Reducer {
         v = io.getInt();
         e = io.getInt();
         c = io.getInt();
-
-        edges = new Edge[e];
+        boi = new StringBuilder();
         nonSoloVerticies = new ArrayList<>();
         int[] edgesList = new int[e*2];
+
         for (int i = 0; i < e * 2; i++) {
             int a = io.getInt();
             if (!nonSoloVerticies.contains(a))
                 nonSoloVerticies.add(a);
             edgesList[i] = a;
         }
+        int oldV = v;
+        v = nonSoloVerticies.size();
+        nORoles = v + 3;
+        nOScenes = e + 2;
+        nOactors = c + 3;
+        printFirstThree();
+        printDivaRoles();
 
+        printRoles();
         int sub = 0;
         ArrayList<Integer> typHash = new ArrayList<>();
         typHash.add(0);
-        for (int i = 1; i <= v; i++) {
+        for (int i = 1; i <= oldV; i++) {
             if (!nonSoloVerticies.contains(i)) {
                 sub++;
             }
             typHash.add(i - sub);
         }
+        printDivaScenes();
 
         for (int i = 0; i < e * 2; i +=2) {
-            edges[i/2] = new Edge(typHash.get(edgesList[i]), typHash.get(edgesList[i+1]));
+            printScene(typHash.get(edgesList[i]), typHash.get(edgesList[i+1]));
         }
-
-        v = nonSoloVerticies.size();
+        io.flush();
     }
 
-    private void print() {
-        nORoles = v + 3;
-        nOScenes = e + 2;
-        nOactors = c + 3;
-
-        io.println(nORoles);
-        io.println(nOScenes);
-        io.println(nOactors);
-
-
-        //Roles
-
-        //Divor
+    private void printDivaRoles(){
         io.println(1 + " " + 1);
         io.println(1 + " " + 2);
         io.println(1 + " " + 3);
+    }
 
-        //Alla Andra
-        for (int i = 0; i < v; i++) {
-            io.print(c);
-            for (int j = 4; j <= nOactors; j++) {
-                io.print(" " + j);
-            }
-            io.println();
-        }
+    private void printScene(int a, int b){
 
-        //Scenes
-
-        //Divor
+        io.println(2 + " " + (a + 3) + " " + (b + 3));
+    }
+    private void printDivaScenes(){
         io.println(2 + " " + 1 + " " + 3);
         io.println(2 + " " + 2 + " " + 3);
+    }
 
-        //Alla andra
-        for (Edge edge : edges) {
-            io.println(2 + " " + (edge.getA() + 3) + " " + (edge.getB() + 3));
+    private void printFirstThree(){
+        io.println(nORoles);
+        io.println(nOScenes);
+        io.println(nOactors);
+    }
+    private void printRoles(){
+        for (int i = 0; i < v; i++) {
+
+            boi.append(c);
+            for (int j = 4; j <= nOactors; j++) {
+                boi.append(" ");
+                boi.append(j);
+            }
+            io.println(boi);
         }
-        io.flush();
     }
 }
